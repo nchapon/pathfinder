ec7b17b1bbf0fd2c10e90cb56874affd13fd394c
Nicolas Chapon <nchapon@gmail.com>
2013-07-11 17:14:44 +0200

    Add cost functions

--- src/pathfinder/core.clj
+++ src/pathfinder/core.clj
@@ -1,12 +1,24 @@
 (ns pathfinder.core)
 
 
-
-
-
 (defn neighbors
   ([size xy] (neighbors [[-1 0] [1 0] [0 -1] [0 1]] size xy))
   ([deltas size xy]
      (filter (fn [new-xy]
                (every? #(< -1 % size) new-xy))
              (map #(map + xy %) deltas))))
+
+(defn estimate-cost
+  [step-cost-est size x y]
+  (* step-cost-est
+     (- (+ size size) x y 2)))
+
+(defn path-cost
+  [node-cost cheapest-nbr]
+  (+ node-cost
+     (:cost cheapest-nbr 0)))
+
+(defn total-cost
+  [new-cost step-cost-est size x y]
+  (+ new-cost
+     (estimate-cost step-cost-est size x y)))
--- test/pathfinder/core_test.clj
+++ test/pathfinder/core_test.clj
@@ -10,9 +10,16 @@
             [1 1 1 1 1]])
 
 
-
-
-
 (fact (neighbors 5 [0 0]) => (just [[1 0] [0 1]]))
 
 (fact (neighbors 5 [4 4]) => (just [[3 4] [4 3]]))
+
+(fact (estimate-cost 900 5 0 0) => 7200)
+
+(fact (estimate-cost 900 5 4 4) => 0)
+
+(fact (path-cost 900 {:cost 1}) => 901)
+
+(fact (total-cost 0 900 5 0 0) => 7200)
+
+(fact (total-cost 1000 900 5 3 4) => 1900)
